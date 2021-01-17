add0c98137d2062f86a2d6500146303a5255961f
Eneida Gonzales <e.gonzaleslema@gmail.com>
2021-01-17 19:48:08 -0400

    adicionando imagen

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,18 @@
 import 'package:flutter/material.dart';
  
+import 'package:disenios/src/pages/basico.dart';
 void main() => runApp(MyApp());
  
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Material App Bar'),
-        ),
-        body: Center(
-          child: Container(
-            child: Text('Hello World'),
-          ),
-        ),
-      ),
+      debugShowCheckedModeBanner: false,
+      title: 'Diseños',
+      initialRoute: 'basico',
+      routes: {
+        'basico'  : (BuildContext context) => BasicoPage()
+      },
     );
   }
 }
